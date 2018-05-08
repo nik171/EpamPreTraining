04af06d2fcc9205d50970c505e5593523f8c77b5
nik171 <filatovni@mail.ru>
2018-05-08 11:24:32 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class Test {
+    public static void main(String[] args){
+        AnswerPrinter.printAnswer(ArrayWorker.findMinMaxValue(10, 0, 10));
+        AnswerPrinter.printAnswer(ArrayWorker.calcMediumValues(10, 0, 10));
+    }
+}
