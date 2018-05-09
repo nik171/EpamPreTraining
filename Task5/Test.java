8e20d417d66b32898ee3fca128911ff9f4bad6c0
nik171 <filatovni@mail.ru>
2018-05-09 21:02:59 +0300

    Update Test.java

--- Task5/Test.java
+++ Task5/Test.java
@@ -1,8 +1,14 @@
 package arrays;
 
 public class Test {
-    public static void main(String[] args){
-        AnswerPrinter.printAnswer(ArrayWorker.findMinMaxValue(10, 0, 10));
-        AnswerPrinter.printAnswer(ArrayWorker.calcMediumValues(10, 0, 10));
+    public static void main(String[] args) {
+        double[] array = ArrayBuilder.buildArray(10, -10, 10);
+        AnswerPrinter.printAnswer("\nTask1:\n" + ArrayWorker.findMinMaxValue(array));
+        AnswerPrinter.printAnswer("\nTask2:\n" + ArrayWorker.calcMediumValues(array));
+        AnswerPrinter.printAnswer("\nTask3:\n" + ArrayWorker.isOrdered(array) + "\n");
+        AnswerPrinter.printAnswer("\nTask4:\nlocal min(max) = " + ArrayWorker.findLocalMinMax(array) + "\n");
+        AnswerPrinter.printAnswer("\nTask5:\n" + "reverse " + ArrayWorker.reverseArray(array));
+        AnswerPrinter.printAnswer("\nTask10_1:\nmin = " + Task10.findMinElement(array) + "\n");
+        AnswerPrinter.printAnswer("\nTask10_2:\nsum between first and last positive = " + Task10.calcSum(array));
     }
 }
