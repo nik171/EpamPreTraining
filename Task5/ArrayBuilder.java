fa36eefc4ca1e6b87529f944fc051036211d9e55
nik171 <filatovni@mail.ru>
2018-05-09 21:05:10 +0300

    Update ArrayBuilder.java

--- Task5/ArrayBuilder.java
+++ Task5/ArrayBuilder.java
@@ -4,14 +4,13 @@ import java.util.Random;
 
 public class ArrayBuilder {
 
-    public static double[] buildArray(int n, double minVal, double maxVal){
+    public static double[] buildArray(int n, double minVal, double maxVal) {
         final Random random = new Random();
         double[] array = new double[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             array[i] = minVal + (maxVal - minVal) * random.nextDouble();
         }
 
         return array;
     }
-
 }
