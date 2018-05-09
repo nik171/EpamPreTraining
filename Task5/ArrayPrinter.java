9e13f46862f3f0e6a1484e6eb4a0f7ae63be9b42
nik171 <filatovni@mail.ru>
2018-05-09 21:04:36 +0300

    Update ArrayPrinter.java

--- Task5/ArrayPrinter.java
+++ Task5/ArrayPrinter.java
@@ -1,9 +1,9 @@
 package arrays;
 
-public class ArrayPrinter{
-    public static String printArray(double[] array){
-        String strArray = "";
-        for(int i = 0; i < array.length; i++){
+public class ArrayPrinter {
+    public static String printArray(double[] array) {
+        String strArray = "array:\n";
+        for (int i = 0; i < array.length; i++) {
             strArray += array[i] + " ";
         }
         strArray += "\n";
