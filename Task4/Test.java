16799032827baad0002ce56b8150c9311b439747
nik171 <filatovni@mail.ru>
2018-05-09 21:20:32 +0300

    Update Test.java

--- Task4/Test.java
+++ Task4/Test.java
@@ -4,11 +4,11 @@ public class Test {
     public static void main(String[] args) {
         Print.printAnswer(HeadsOrTails.countHeadsAndTails(1000));
         Print.printAnswer(NumberWorker.calcMaxDigit(-456710));
-        Print.printAnswer(NumberWorker.isPalindrome(1234321));
-        Print.printAnswer((NumberWorker.isPrime(1113)));
+        Print.printAnswer("\n***** Task2_2 *****\npalindrome: " + NumberWorker.isPalindrome(1234321));
+        Print.printAnswer(("\n***** Task2_3 *****\nprime: " + NumberWorker.isPrime(1113)));
         Print.printAnswer((NumberWorker.calcPrimeDivisors(1113)));
         Print.printAnswer(NumberWorker.calcGCDandLCM(256, 1024));
         Print.printAnswer(NumberWorker.calcDifferentDigits(1213141578));
-        Print.printAnswer(PerfectNumbers.isPerfectNumber(33550336));
+        Print.printAnswer("\n***** Task3 *****\nperfect: " + PerfectNumbers.isPerfectNumber(33550336));
     }
 }
