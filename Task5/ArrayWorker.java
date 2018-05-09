75e476fc00d8a99a9c965795810ed74bb65a96d9
nik171 <filatovni@mail.ru>
2018-05-09 21:03:45 +0300

    Update ArrayWorker.java

--- Task5/ArrayWorker.java
+++ Task5/ArrayWorker.java
@@ -1,16 +1,14 @@
 package arrays;
 
 public class ArrayWorker {
-    public static String findMinMaxValue(int n, double minValue, double maxValue){
-        double[] array  =ArrayBuilder.buildArray(n, minValue, maxValue);
+    public static String findMinMaxValue(double[] array) {
         double min = array[0];
         double max = min;
 
-        for(int i = 0; i < array.length; i++){
-            if(array[i] > max){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
                 max = array[i];
-            }
-            else if(array[i] < min){
+            } else if (array[i] < min) {
                 min = array[i];
             }
         }
@@ -18,17 +16,46 @@ public class ArrayWorker {
         return ArrayPrinter.printArray(array) + "min = " + min + "\nmax = " + max + "\n";
     }
 
-    public static String calcMediumValues(int n, double minValue, double maxValue){
-        double[] array  =ArrayBuilder.buildArray(n, minValue, maxValue);
+    public static String calcMediumValues(double[] array) {
         double sum = 0, mult = 1;
-        double power = (double)1 / n;
+        double power = (double) 1 / array.length;
 
-        for(int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
             mult *= array[i];
         }
 
-        return ArrayPrinter.printArray(array) + "arithmetic = " + (sum / n) +
+        return "arithmetic = " + (sum / array.length) +
                 "\ngeometric = " + Math.pow(mult, power) + "\n";
+    }
+
+    public static boolean isOrdered(double[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (((array[i] > array[i - 1]) && (array[i + 1] < array[i])) ||
+                    ((array[i] < array[i - 1]) && (array[i + 1] > array[i]))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static double findLocalMinMax(double[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (((array[i] > array[i - 1]) && (array[i + 1] < array[i])) ||
+                    ((array[i] < array[i - 1]) && (array[i + 1] > array[i]))) {
+                return array[i];
+            }
+        }
+
+        return -1;
+    }
+
+    public static String reverseArray(double[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            array[i] = (array[i] + array[array.length - i - 1]) - (array[array.length - i - 1] = array[i]);
+        }
+
+        return ArrayPrinter.printArray(array);
     }
 }
