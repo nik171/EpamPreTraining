b4111bb0f4cd47d9024466eb91e8e29770dfcb14
nik171 <filatovni@mail.ru>
2018-05-09 21:23:25 +0300

    Update HeadsOrTails.java

--- Task4/HeadsOrTails.java
+++ Task4/HeadsOrTails.java
@@ -3,14 +3,13 @@ package loop;
 import java.util.Random;
 
 public class HeadsOrTails {
-    public static final int HEAD = 1;
 
     public static String countHeadsAndTails(long n) {
         final Random random = new Random();
         long numberOfHeads = 0;
         for (long i = 0; i < n; i++) {
-            int randNumber = random.nextInt(2);
-            if (randNumber == HEAD) {
+            boolean randNumber = random.nextBoolean();
+            if (randNumber == true) {
                 numberOfHeads++;
             }
         }
